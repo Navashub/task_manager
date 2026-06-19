697be1d7916c76aa4a14c98cb753e92b95bdac07
Navashub <navasmuller01@gmail.com>
2026-06-19 08:48:00 +0300

    Add the tasks endpoints

---
+++ routers/tasks.py
@@ -0,0 +1,40 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import List
+import crud, schemas
+from database import get_db
+
+router = APIRouter(prefix="/tasks", tags=["Tasks"])
+
+@router.post("/", response_model=schemas.TaskResponse)
+def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
+    return crud.create_task(db, task)
+
+
+@router.get("/", response_model=List(schemas.TaskResponse))
+def get_tasks(db: Session = Depends(get_db)):
+    return crud.get_tasks(db)
+
+
+@router.get("/{task_id}", response_model=schemas.TaskResponse)
+def get_task(task_id: int, db: Session = Depends(get_db)):
+    task = crud.get_task(db, task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return task 
+
+
+@router.put("/{task_id}", response_model=schemas.TaskResponse)
+def update_task(task_id: int, data: schemas.TaskUpdate, db: Session = Depends(get_db)):
+    task = crud.update_task(db, task_id, data)
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return task 
+
+
+@router.delete("/{task_id}")
+def delete_task(task_id: int, db: Session =  Depends(get_db)):
+    task = crud.delete_task(db, task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return {"message": "Task deleted successfully"}
